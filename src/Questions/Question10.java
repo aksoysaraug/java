584bdbec11743a487148ab1add28cecf515bce32
Ummu Gulsum Aksoy <eczglsm89@gmail.com>
2022-04-13 02:46:59 -0400

    queation10

---
+++ src/Questions/Question10.java
@@ -0,0 +1,27 @@
+package Questions;
+
+public class Question10 {
+        /*
+How we can prevent an object creation except defining it abstract.
+Also This class must allow only one copy of itself.
+There must be only one copy of it and every other objects and classes
+in the project must use that copy.
+ */
+            private static Question10 instance = new Question10();
+
+            public String user;
+
+            public static void main(String[] args) {
+                Question10 q1 = new Question10();
+            }
+
+            public static Question10 getInstance(){
+                return instance;
+            }
+
+            private Question10() {
+
+            }
+
+
+}
