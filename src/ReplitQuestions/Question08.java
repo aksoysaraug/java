f72d8aae4ab2ffcad7a972035d9f4042e809eea3
Ummu Gulsum Aksoy <eczglsm89@gmail.com>
2022-05-01 15:26:40 -0400

    Q08

---
+++ src/ReplitQuestions/Question08.java
@@ -0,0 +1,22 @@
+package ReplitQuestions;
+
+public class Question08 {
+    public static void main(String[] args) {
+        /*
+        Dikdortgenin alanini ve cevresini hesaplayan java kodunu yaziniz.
+        A=5
+        B=3
+        Ipucu: Dikdortgenin Cevresi : 2*(A+B);
+        Dikdortgenin Alani : A * B ;
+        Ornek Cikti:
+        Dikdortgenin Cevresi : 16
+        Dikdortgenin Alani : 15
+         */
+        int a= 5;
+        int b= 3;
+        System.out.println("Dikdortgenin Cevresi :"+2*(a+b));
+        System.out.println("Dikdortgenin Alani :"+a*b);
+
+
+    }
+}
